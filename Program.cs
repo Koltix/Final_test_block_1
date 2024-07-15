5d1b42e1c0ac3cb5276f177c36eff9e3adc1b600
Koltix <159639837+Koltix@users.noreply.github.com>
2024-07-16 00:11:04 +0300

    Update Program.cs
    Внесены изменения в программу

--- Program.cs
+++ Program.cs
@@ -1,10 +1,71 @@
 using System;
+class Program
+{
+    static void Main()
+    {
+        int arraySize = RequestArraySize();
+        string[] inputStrings = ReadInputStrings(arraySize);
+        string[] suitableStrings = FilterStrings(inputStrings);
+        DisplayResults(inputStrings, suitableStrings);
+    }
 
-     class Program
-     {
-         static void Main()
-         {
-             Console.WriteLine("Hello, World!");
-         }
-     }
+    static int RequestArraySize()
+    {
+        Console.Write("Введите количество строк в массиве: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static string[] ReadInputStrings(int arraySize)
+    {
+        string[] strings = new string[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            Console.Write($"Введите строку {i + 1}: ");
+            strings[i] = Console.ReadLine();
+        }
+        return strings;
+    }
+
+    static string[] FilterStrings(string[] inputStrings)
+    {
+        int suitableCount = CountSuitableStrings(inputStrings);
+        string[] suitableStrings = new string[suitableCount];
+        int index = 0;
+        foreach (string str in inputStrings)
+        {
+            if (str.Length <= 3)
+            {
+                suitableStrings[index] = str;
+                index++;
+            }
+        }
+        return suitableStrings;
+    }
+
+    static int CountSuitableStrings(string[] strings)
+    {
+        int count = 0;
+        foreach (string str in strings)
+        {
+            if (str.Length <= 3)
+                count++;
+        }
+        return count;
+    }
+
+    static void DisplayResults(string[] inputStrings, string[] suitableStrings)
+    {
+        Console.WriteLine("Исходный массив строк: ");
+        foreach (string str in inputStrings)
+        {
+            Console.WriteLine(str);
+        }
+
+        Console.WriteLine("Новый массив с подходящими строками (длина <= 3 символов):");
+        foreach (string str in suitableStrings)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
      
